0e50724fc21e98c40b7072f6ba501537e0e76014
mojhehh <sebmojheh@gmail.com>
2026-01-16 07:25:04 -0800

    Rebrand to UnfilteredGPT with expressive personality

--- app.js
+++ app.js
@@ -547,7 +547,7 @@ class UnfilteredAI {
             ? '<svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>'
             : '<svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"/></svg>';
         
-        const roleName = role === 'user' ? 'You' : 'Unfiltered';
+        const roleName = role === 'user' ? 'You' : 'UnfilteredGPT';
 
         messageDiv.innerHTML = `
             <div class="message-inner">
